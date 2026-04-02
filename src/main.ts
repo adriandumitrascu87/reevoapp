fc060acd63cdaca4fee86622a1e1184c03e0907c
Adi <adriandumitrascu87@gmail.com>
2026-04-02 13:05:17 +0300

    created pixi app

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,21 @@
-console.log("OLA");
+
+import { app } from "./app";
+import "./global.css";
+import { GameScene } from "./scenes/GameScene";
+import { PALETTE } from "./settings/palette";
+
+(async () => {
+ 
+  await app.init({
+    resizeTo: window,
+    preference: "webgl",
+    antialias: true,
+    backgroundColor: PALETTE.app.background,
+    resolution: window.devicePixelRatio || 1,
+    autoDensity: true,
+  });
+  document.body.appendChild(app.canvas);
+
+  const game = new GameScene();
+  if (game && game.mainContainer) app.stage.addChild(game.mainContainer);
+})();
