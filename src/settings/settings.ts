ee2890a2e7fa84cb0b6fb99ba5ac8c4e9912a8f4
Adi <adriandumitrascu87@gmail.com>
2026-04-02 15:52:36 +0300

    added settings for pools and spawner

--- src/settings/settings.ts
+++ src/settings/settings.ts
@@ -1,6 +1,12 @@
 export const SETTINGS = {
-    padding:{
-        horizontal:0.05,
-        vertical:0.2
-    }
-}
+  padding: {
+    horizontal: 0.05,
+    vertical: 0.2,
+  },
+  poolSize: {
+    initialSize: 10,
+  },
+  gravity: 2,
+  spawnIntervalMS: 1,
+  shapeCacheSize: 20,
+};
