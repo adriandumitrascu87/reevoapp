9079f8539eff16b873a51ba6d4836a38b3da00d8
Adi <adriandumitrascu87@gmail.com>
2026-04-02 15:50:28 +0300

    created shapeFactory cache

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,10 @@
-
 import { app } from "./app";
+import { ShapeFactory } from "./factory/ShapeFactory";
 import "./global.css";
 import { GameScene } from "./scenes/GameScene";
 import { PALETTE } from "./settings/palette";
 
 (async () => {
- 
   await app.init({
     resizeTo: window,
     preference: "webgl",
@@ -16,6 +15,10 @@ import { PALETTE } from "./settings/palette";
   });
   document.body.appendChild(app.canvas);
 
+  // console.log("screen:", app.screen.width, app.screen.height);
+  // console.log("window:", window.innerWidth, window.innerHeight);
+
+  ShapeFactory.generateCache();
   const game = new GameScene();
   if (game && game.mainContainer) app.stage.addChild(game.mainContainer);
 })();
